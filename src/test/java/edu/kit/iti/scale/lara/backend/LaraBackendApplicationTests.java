3b95ff8b6b62ccdd48aeb59ba881d7c6d3e7a574
Gregyyy <gregyyy@pm.me>
2023-01-16 16:20:37 +0100

    Add Spring Boot project

---
+++ src/test/java/edu/kit/iti/scale/lara/backend/LaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.iti.scale.lara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
