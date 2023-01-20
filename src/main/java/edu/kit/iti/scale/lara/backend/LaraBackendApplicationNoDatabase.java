dcc5d5dc2347ebdd6ecd770f2d184cd3b5aa825e
Gregyyy <gregyyy@pm.me>
2023-01-20 17:01:02 +0100

    Hopefully fix RSA properties

--- src/main/java/edu/kit/iti/scale/lara/backend/LaraBackendApplicationNoDatabase.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/LaraBackendApplicationNoDatabase.java
@@ -1,10 +1,12 @@
 package edu.kit.iti.scale.lara.backend;
 
+import edu.kit.iti.scale.lara.backend.controller.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 
 @Profile("nodb")
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Profile;
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
 })
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class LaraBackendApplicationNoDatabase {
 
     public static void main(String[] args) {
