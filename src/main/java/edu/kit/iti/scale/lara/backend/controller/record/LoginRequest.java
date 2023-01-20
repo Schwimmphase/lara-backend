d814786d5a42f7870e95dd086001a21b08efbc1b
Gregyyy <gregyyy@pm.me>
2023-01-20 13:23:58 +0100

    Implement auth

--- src/main/java/edu/kit/iti/scale/lara/backend/controller/record/LoginRequest.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/record/LoginRequest.java
@@ -1,4 +1,4 @@
 package edu.kit.iti.scale.lara.backend.controller.record;
 
-public record LoginRequest() {
+public record LoginRequest(String userId, String password) {
 }
--- src/main/java/edu/kit/iti/scale/lara/backend/controller/repository/UserRepository.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.kit.iti.scale.lara.backend.controller.repository;
 
+import edu.kit.iti.scale.lara.backend.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Void, Void> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
