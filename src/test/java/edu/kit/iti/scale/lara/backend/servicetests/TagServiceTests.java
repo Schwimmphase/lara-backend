516cc85d05e2cf007cf367cdab77676160176028
Paul <ukgcc@student.kit.edu>
2023-01-31 15:05:16 +0100

    TagServiceTests without tip

--- src/test/java/edu/kit/iti/scale/lara/backend/servicetests/TagServiceTests.java
+++ src/test/java/edu/kit/iti/scale/lara/backend/servicetests/TagServiceTests.java
@@ -1,12 +1,12 @@
 package edu.kit.iti.scale.lara.backend.servicetests;
 
 import edu.kit.iti.scale.lara.backend.InMemoryTest;
-import edu.kit.iti.scale.lara.backend.TestInstanceProvider;
 import edu.kit.iti.scale.lara.backend.controller.service.TagService;
 import edu.kit.iti.scale.lara.backend.exceptions.NotInDataBaseException;
 import edu.kit.iti.scale.lara.backend.exceptions.WrongUserException;
 import edu.kit.iti.scale.lara.backend.model.research.Research;
 import edu.kit.iti.scale.lara.backend.model.research.paper.savedpaper.Tag;
+import edu.kit.iti.scale.lara.backend.model.user.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,20 +19,21 @@ public class TagServiceTests {
     @Autowired
     private TagService tagService;
 
-    private TestInstanceProvider tip = new TestInstanceProvider();
-
-    private Research research = tip.getResearch2();
+    @Autowired
+    private Research persistentResearch1;
+    @Autowired
+    private User persistentUser1;
 
     @Test
     public void createAndGetTag() {
-        Tag tag1 = tagService.createTag("#FF0000", "Test-tag-1", research);
-        Tag tag2 = tagService.createTag("#00FF00", "Test-tag-2", research);
-        Tag tag3 = tagService.createTag("#0000FF", "Test-tag-3", research);
+        Tag tag1 = tagService.createTag("#FF0000", "Test-tag-1", persistentResearch1);
+        Tag tag2 = tagService.createTag("#00FF00", "Test-tag-2", persistentResearch1);
+        Tag tag3 = tagService.createTag("#0000FF", "Test-tag-3", persistentResearch1);
 
         try {
-            Tag returnedTag1 = tagService.getTag(tag1.getId(), tip.getUser2());
-            Tag returnedTag2 = tagService.getTag(tag2.getId(), tip.getUser2());
-            Tag returnedTag3 = tagService.getTag(tag3.getId(), tip.getUser2());
+            Tag returnedTag1 = tagService.getTag(tag1.getId(), persistentUser1);
+            Tag returnedTag2 = tagService.getTag(tag2.getId(), persistentUser1);
+            Tag returnedTag3 = tagService.getTag(tag3.getId(), persistentUser1);
 
             Assertions.assertThat(returnedTag1).isEqualTo(tag1);
             Assertions.assertThat(returnedTag2).isEqualTo(tag2);
@@ -43,7 +44,7 @@ public class TagServiceTests {
             System.out.println("User has no Tag with given Id");
             boolean exceptionThrown = false;
             try {
-                tagService.getTag("4", tip.getUser1());
+                tagService.getTag("4", persistentUser1);
             } catch (NotInDataBaseException ee) {
                 exceptionThrown = true;
             } catch (WrongUserException eee) {
@@ -55,18 +56,18 @@ public class TagServiceTests {
 
     @Test
     public void testGetTags() {
-        Tag tag1 = tagService.createTag("#FF0000", "Test-tag-1", research);
-        Tag tag2 = tagService.createTag("#00FF00", "Test-tag-2", research);
-        Tag tag3 = tagService.createTag("#0000FF", "Test-tag-3", research);
+        Tag tag1 = tagService.createTag("#FF0000", "Test-tag-1", persistentResearch1);
+        Tag tag2 = tagService.createTag("#00FF00", "Test-tag-2", persistentResearch1);
+        Tag tag3 = tagService.createTag("#0000FF", "Test-tag-3", persistentResearch1);
 
-        List<Tag> tags = tagService.getTags(research);
+        List<Tag> tags = tagService.getTags(persistentResearch1);
 
         Assertions.assertThat(tags).isEqualTo(List.of(tag1, tag2, tag3));
     }
 
     @Test
     public void testUpdateTag() {
-        Tag tag = tagService.createTag("#FF0000", "Test-tag", research);
+        Tag tag = tagService.createTag("#FF0000", "Test-tag", persistentResearch1);
 
         tagService.updateTag(tag, "New-Name", "#0000FF");
 
@@ -76,10 +77,10 @@ public class TagServiceTests {
 
     @Test
     public void testDeleteTag() {
-        Tag tag = tagService.createTag("#FF0000", "Test-tag", research);
+        Tag tag = tagService.createTag("#FF0000", "Test-tag", persistentResearch1);
 
         try {
-            Assertions.assertThat(tagService.getTag(tag.getId(), tip.getUser2())).isEqualTo(tag);
+            Assertions.assertThat(tagService.getTag(tag.getId(), persistentUser1)).isEqualTo(tag);
         } catch (WrongUserException | NotInDataBaseException e) {
             System.out.println("This shouldn´t happen");
         }
@@ -89,7 +90,7 @@ public class TagServiceTests {
         boolean exceptionThrown = false;
 
         try {
-            tagService.getTag(tag.getId(), tip.getUser2());
+            tagService.getTag(tag.getId(), persistentUser1);
         } catch (NotInDataBaseException ee) {
             exceptionThrown = true;
         } catch (WrongUserException eee) {
