9cf29e1190555f8f12157c3511d017b7bd2bb0aa
Gregyyy <gregyyy@pm.me>
2023-02-03 17:50:55 +0100

    Change separator to $

--- src/main/java/edu/kit/iti/scale/lara/backend/api/IdParser.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/api/IdParser.java
@@ -1,15 +1,17 @@
 package edu.kit.iti.scale.lara.backend.api;
 
+import java.util.regex.Pattern;
+
 public class IdParser {
 
-    private static final String SEPARATOR = "#";
+    private static final String SEPARATOR = "$";
 
     public String encodedId(String apiPrefix, String id) {
         return apiPrefix + SEPARATOR + id;
     }
 
     public String decodedId(String id) {
-        return id.split(SEPARATOR)[1];
+        return id.split(Pattern.quote(SEPARATOR))[1];
     }
 
 }
