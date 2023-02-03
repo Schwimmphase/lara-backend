14be59f237f3e95880a03ce1e1e4ecaeab9ec0e8
Gregyyy <gregyyy@pm.me>
2023-02-03 16:39:27 +0100

    Implement user service

--- src/main/java/edu/kit/iti/scale/lara/backend/controller/controller/UserInterceptor.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/controller/UserInterceptor.java
@@ -1,4 +1,36 @@
 package edu.kit.iti.scale.lara.backend.controller.controller;
 
-public class UserInterceptor {
+import edu.kit.iti.scale.lara.backend.controller.service.UserService;
+import edu.kit.iti.scale.lara.backend.exceptions.NotInDataBaseException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.security.Principal;
+
+@Component
+@RequiredArgsConstructor
+public class UserInterceptor implements HandlerInterceptor {
+
+    private final UserService userService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+        Principal principal = request.getUserPrincipal();
+
+        if (principal != null) {
+            try {
+                request.setAttribute("user", userService.getUserById(principal.getName()));
+            } catch (NotInDataBaseException e) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+            }
+        }
+
+        return true;
+    }
 }
