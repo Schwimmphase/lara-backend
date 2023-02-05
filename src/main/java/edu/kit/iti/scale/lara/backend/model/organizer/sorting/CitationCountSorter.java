d3c6ca13705f9cc85f01ea9b354d699818f93c51
Paul <ukgcc@student.kit.edu>
2023-02-05 20:40:12 +0100

    implemented the Sorters

--- src/main/java/edu/kit/iti/scale/lara/backend/model/organizer/sorting/CitationCountSorter.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/model/organizer/sorting/CitationCountSorter.java
@@ -1,14 +1,16 @@
 package edu.kit.iti.scale.lara.backend.model.organizer.sorting;
 
 import edu.kit.iti.scale.lara.backend.model.research.paper.Paper;
+import org.springframework.security.core.parameters.P;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CitationCountSorter extends Sorter {
 
     @Override
     public List<Paper> organize(List<Paper> papers) {
-        //todo
-        return null;
+        papers.sort(Comparator.comparing(Paper::getCitationCount));
+        return papers;
     }
 }
--- src/main/java/edu/kit/iti/scale/lara/backend/model/organizer/sorting/YearSorter.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/model/organizer/sorting/YearSorter.java
@@ -2,12 +2,13 @@ package edu.kit.iti.scale.lara.backend.model.organizer.sorting;
 
 import edu.kit.iti.scale.lara.backend.model.research.paper.Paper;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class YearSorter extends Sorter {
     @Override
     public List<Paper> organize(List<Paper> papers) {
-        //todo
-        return null;
+        papers.sort(Comparator.comparing(Paper::getYearPublished));
+        return papers;
     }
 }
