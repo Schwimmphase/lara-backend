92937b7fde404e0c1351c1d90042f214f5d86ce3
Gregyyy <gregyyy@pm.me>
2023-02-06 10:16:18 +0100

    Rename userCategory to usercategory

--- src/main/java/edu/kit/iti/scale/lara/backend/controller/request/UserRequest.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/request/UserRequest.java
@@ -3,5 +3,5 @@ package edu.kit.iti.scale.lara.backend.controller.request;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public record UserRequest(@NotNull String username, @Nullable String password, @NotNull String userCategory) {
+public record UserRequest(@NotNull String username, @Nullable String password, @NotNull String usercategory) {
 }
