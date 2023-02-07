8f71948defdd6d40c169c2ef22455c7861be1dec
Gregyyy <gregyyy@pm.me>
2023-02-07 14:47:41 +0100

    Add component annotation

--- src/main/java/edu/kit/iti/scale/lara/backend/controller/config/converter/RecommendationMethodConverter.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/config/converter/RecommendationMethodConverter.java
@@ -1,14 +1,14 @@
 package edu.kit.iti.scale.lara.backend.controller.config.converter;
 
 import edu.kit.iti.scale.lara.backend.controller.RecommendationMethod;
-
 import org.jetbrains.annotations.NotNull;
-
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 
+@Component
 public class RecommendationMethodConverter implements Converter<String, RecommendationMethod> {
     @Override
     public RecommendationMethod convert(@NotNull String source) {
