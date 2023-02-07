82b6c557358a93b50dc91d02e39fb9129959213d
Paul <ukgcc@student.kit.edu>
2023-02-07 14:14:44 +0100

    RecommendationMethodConverter

---
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/config/converter/RecommendationMethodConverter.java
@@ -0,0 +1,20 @@
+package edu.kit.iti.scale.lara.backend.controller.config.converter;
+
+import edu.kit.iti.scale.lara.backend.controller.RecommendationMethod;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Arrays;
+
+
+public class RecommendationMethodConverter implements Converter<String, RecommendationMethod> {
+    @Override
+    public RecommendationMethod convert(@NotNull String source) {
+        if (Arrays.stream(RecommendationMethod.values()).noneMatch(recommendationMethod -> recommendationMethod.name().equals(source.toUpperCase()))) {
+            throw new IllegalArgumentException("Invalid recommendation method: " + source.toUpperCase());
+        }
+        return RecommendationMethod.valueOf(source.toUpperCase());
+    }
+}
