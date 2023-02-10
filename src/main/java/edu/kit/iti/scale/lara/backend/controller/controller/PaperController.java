41f26cef002570a9cc3d685713ecaed0fc0ff90a
uruox <uruox@student.kit.edu>
2023-02-10 11:17:43 +0100

    changed comment to commentRequest in paperComment

--- src/main/java/edu/kit/iti/scale/lara/backend/controller/controller/PaperController.java
+++ src/main/java/edu/kit/iti/scale/lara/backend/controller/controller/PaperController.java
@@ -117,9 +117,9 @@ public class PaperController {
     @PatchMapping("/{id}/comment")
     public ResponseEntity<Void> paperComment(@PathVariable @NotNull String id,
                                              @RequestParam @NotNull String researchId,
-                                             @RequestBody @NotNull String comment,
+                                             @RequestBody @NotNull Map<String, String> commentRequest,
                                              @RequestAttribute("user") User user) {
-
+        String comment = commentRequest.get("comment");
         try {
             Research research = researchService.getResearch(researchId, user);
             Paper paper = paperService.getPaper(id);
